07557d696d32cad9a11cac28eafabcbdd0ff6782
destawako <75337634+destawako@users.noreply.github.com>
2021-06-21 20:39:11 -0400

    fibadded

---
+++ computeFib/fibonacci.js
@@ -0,0 +1,19 @@
+const fib=function(num){
+  
+    //   if(num<0)
+    //    {
+    //        num=num*-1;
+    //    }
+    if  (num<0)
+       {
+       return (Math.pow(-1,num+1) * fib(Math.abs(num)))*-1;
+       }
+        if(num==0 || num==1)
+        return 1;
+    else {
+            return fib(num-1)+fib(num-2);
+        }
+    
+    
+    }
+    console.log("fibonnaci of number is 8",fib((10)));
